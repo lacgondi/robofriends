1be67309ba17938128a15eb4ca3275d43cd2f831
László Gondi <gondilacika03@gmail.com>
2020-05-01 20:18:13 +0200

    why would it work on the first try

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,8 @@ class App extends Component{
         const filteredRobots = robots.filter(robot =>{
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-        return 
+        return !robots.length ?
+        <h1>Loading</h1> :
         (
             <div className='tc'>
                 <h1 className='f2'>RoboFriends</h1>
